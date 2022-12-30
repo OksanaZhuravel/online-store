4ebc64fab201fa79690b968a6775ce44e67a636d
OksanaZhuravel <opuseva@mail.ru>
2022-12-30 21:02:37 +0400

    feat: add basic cart style

--- src/core/tempates/page.ts
+++ src/core/tempates/page.ts
@@ -1,7 +1,7 @@
 abstract class Page {
     protected container: HTMLElement = document.body;
     constructor(public id: string) {
-        this.container = document.createElement('div');
+        this.container = document.createElement('section');
         this.container.id = id;
     }
 
@@ -10,12 +10,17 @@ abstract class Page {
         title.innerHTML = text;
         return title;
     }
-    protected createPage(text: string) {
-        const title = document.createElement('div');
+    // protected createPage(text: string) {
+    //     const title = document.createElement('div');
+    //     title.innerHTML = text;
+    //     return title;
+    // }
+    protected createPage(tagName: string, className: string, text: string) {
+        const title = document.createElement(tagName);
+        title.className = className;
         title.innerHTML = text;
         return title;
     }
-
     render() {
         return this.container;
     }
